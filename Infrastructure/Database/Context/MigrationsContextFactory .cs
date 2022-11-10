45f09b927cc0e99565f6935023e289197621127f
dimitkos <User1@DESKTOP-D6C1HHM>
2022-11-10 21:37:12 +0200

    Add migration context factory

---
+++ Infrastructure/Database/Context/MigrationsContextFactory .cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Database.Context
+{
+    class MigrationsContextFactory : IDesignTimeDbContextFactory<PlayerDbContext>
+    {
+        public PlayerDbContext CreateDbContext(string[] args)
+        {
+#warning try to add ioptions to get connection string
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.GetFullPath(@"../Api/"))
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.Development.json", optional: false)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<PlayerDbContext>();
+#warning add conection string
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString(""));
+
+            return new PlayerDbContext(optionsBuilder.Options);
+        }
+    }
+}
